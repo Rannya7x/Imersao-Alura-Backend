ad33efe152a85c2368acf4f8aefe195ee6bc89d9
Rannya7x <58481179+Rannya7x@users.noreply.github.com>
2024-12-02 18:03:25 +0000

    Update postsRoutes.js

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { listarPosts, enviarPosts, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
 
 const corsOptions = {
-    origin: "https://rannya7x.github.io/",
+    origin: "https://rannya7x.github.io",
     optionsSuccessStatus: 200
 }
 
